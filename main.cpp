64c72c67c2fccec5924b299dedd71fcda77ecf25
roman-zm <roman.zm98@gmail.com>
2017-01-31 00:11:07 +0300

    Минор, транспонирование, нахождение определителя, обратной матрицы

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#define matrix vector<vector<double> >
+#define matrix vector<vector<double>>
 
 #include <algorithm>
+#include <cmath>
 #include <iomanip>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-matrix enterMatr()
-{
+matrix enterMatr() {
     matrix matr;
     size_t a, b;
     cout << "Enter a and b" << endl;
@@ -28,8 +28,7 @@ matrix enterMatr()
     return matr;
 }
 
-void printMatr(matrix matr)
-{
+void printMatr(matrix matr) {
     for (size_t i = 0; i < matr.size(); i++) {
         for (size_t j = 0; j < matr[i].size(); j++) {
             cout << std::left << std::setw(5) << matr[i][j];
@@ -38,18 +37,15 @@ void printMatr(matrix matr)
     }
 }
 
-void printMatr2(matrix matr)
-{
-    for_each(matr.begin(), matr.end(), [](vector<double>& ivec) {
-        for_each(ivec.begin(), ivec.end(), [](double i) {
-            cout << std::left << std::setw(5) << i;
-        });
+void printMatr2(matrix matr) {
+    for_each(matr.begin(), matr.end(), [](vector<double> &ivec) {
+        for_each(ivec.begin(), ivec.end(),
+                 [](double i) { cout << std::left << std::setw(5) << i; });
         cout << endl;
     });
 }
 
-matrix multiplyMatrs(matrix matr1, matrix matr2)
-{
+matrix multiplyMatrs(matrix matr1, matrix matr2) {
     matrix result;
 
     size_t a = matr1.size();
@@ -71,20 +67,105 @@ matrix multiplyMatrs(matrix matr1, matrix matr2)
     return result;
 }
 
-int main(int argc, char* argv[])
-{
+matrix multMatrOnDigit(matrix inMatrix, double number) {
+    matrix response;
+    for (size_t i = 0; i < inMatrix.size(); i++) {
+        vector<double> temp;
+        for (size_t j = 0; j < inMatrix[0].size(); j++) {
+            double dTemp = number * inMatrix[i][j];
+            if (dTemp == -0.0)
+                dTemp = 0;
+            temp.push_back(dTemp);
+        }
+        response.push_back(temp);
+    }
+    return response;
+}
+
+matrix transposeMatrix(matrix inMatrix) {
+    size_t a = inMatrix[0].size();
+    size_t b = inMatrix.size();
+    matrix response;
+
+    for (size_t i = 0; i < a; i++) {
+        vector<double> temp;
+        for (size_t j = 0; j < b; j++) {
+            temp.push_back(inMatrix[j][i]);
+        }
+        response.push_back(temp);
+    }
+    return response;
+}
+
+matrix getMinor(matrix inMatrix, size_t exclRow, size_t exclCol) {
+    size_t a = inMatrix.size() - 1;
+    unsigned ri = 0, ci = 0;
+
+    matrix response;
+    for (size_t i = 0; i < a; i++) {
+        vector<double> temp;
+        if (i >= exclRow)
+            ri = 1;
+        for (size_t j = 0; j < a; j++) {
+            if (j >= exclCol)
+                ci = 1;
+            temp.push_back(inMatrix[i + ri][j + ci]);
+            ci = 0;
+        }
+        response.push_back(temp);
+    }
+    return response;
+}
+
+double det(matrix inMatrix) {
+    double response;
+    if (inMatrix.size() == inMatrix[0].size()) {
+        if (inMatrix.size() == 2) {
+            response = inMatrix[0][0] * inMatrix[1][1] -
+                       inMatrix[0][1] * inMatrix[1][0];
+        } else if (inMatrix.size() == 1) {
+            response = inMatrix[0][0];
+        } else {
+            response = 0;
+            for (size_t i = 0; i < inMatrix.size(); i++) {
+                matrix Minor = getMinor(inMatrix, 0, i);
+                response += inMatrix[0][i] * pow(-1.0, i) * det(Minor);
+            }
+        }
+    }
+    return response;
+}
+
+matrix getInversiveMatrix(matrix inMatrix) {
+    matrix matr;
+    for (size_t i = 0; i < inMatrix.size(); i++) {
+        vector<double> temp;
+        for (size_t j = 0; j < inMatrix[0].size(); j++) {
+            matrix Minor = getMinor(inMatrix, i, j);
+            double dTemp = det(Minor) * pow(-1.0, i + j);
+            if (abs(dTemp) < 0.0000001)
+                dTemp = 0;
+            temp.push_back(dTemp);
+        }
+        matr.push_back(temp);
+    }
+    double number = 1 / det(inMatrix);
+    matr = transposeMatrix(matr);
+    matr = multMatrOnDigit(matr, number);
+    return matr;
+}
+
+int main() {
+
     matrix matr = enterMatr();
-    matrix matr2 = enterMatr();
-
-    cout << "Matrix 1: " << endl;
+    matrix matr2 = getInversiveMatrix(matr);
     printMatr(matr);
+    cout << endl;
+    printMatr(matr2);
+    cout << endl;
 
-    cout << "Matrix 2: " << endl;
-    printMatr2(matr2);
-
-    matrix mulResult = multiplyMatrs(matr, matr2);
-    cout << "Result: " << endl;
-    printMatr2(mulResult);
+    matrix matr3 = multiplyMatrs(matr, matr2);
+    printMatr(matr3);
 
     return 0;
 }
